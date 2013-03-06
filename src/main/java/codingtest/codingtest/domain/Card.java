9d3678ac5223f692626f2ffb6cfa1be29efdc5aa
neil-harmer <neil.harmer@thomsonreuters.com>
2013-03-06 10:33:25 +0000

    Initial commit of the game with no unit tests.

---
+++ src/main/java/codingtest/codingtest/domain/Card.java
@@ -0,0 +1,7 @@
+package codingtest.domain;
+
+/**
+ * This is the domain class that represents a card in a card game.
+ */
+public class Card {
+}
---
+++ src/main/java/codingtest/codingtest/domain/Deck.java
@@ -0,0 +1,7 @@
+package codingtest.domain;
+
+/**
+ * This is the class that represents a deck of cards in a card game.
+ */
+public class Deck {
+}
